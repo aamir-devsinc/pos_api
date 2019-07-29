0713761313741e22a7346d37479c01374c7cf36d
‘Haseeb <abdulhaseeb.khn@gmail.com>
2019-07-29 15:44:59 +0500

    Add categories and product models * Add ancestry gem to support tree structure for nested categories * Add migrations for category and products

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_075913) do
+ActiveRecord::Schema.define(version: 2019_07_27_184231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -22,6 +30,14 @@ ActiveRecord::Schema.define(version: 2019_07_23_075913) do
     t.json "about_you"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
